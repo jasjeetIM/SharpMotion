6712fd41963c09efc1bd51db44fa3026876f2365
Jasjeet Dhaliwal <jdhaliwal@vonmises.cs.umass.edu>
2017-02-19 12:16:20 -0500

    changed call argument order

--- byFrame/sharpMask.m
+++ byFrame/sharpMask.m
@@ -1,16 +1,18 @@
+%Function has not been tested yet due to libtorch.so error
 function status =  sharpMask(v, img_path, res_path, sm_path, n, motion_path)
-status = -1; 
-if exist(img_path, 'file') == 2; 
+status = -1;
+if exist(img_path, 'file') == 2;
   if v == 1
-    status = system(['th '  sm_path  'computeMasks.lua -model ' sm_path '/pretrained/sharpmask/  -res ' res_path ' -v 1 -img ' img_path  ' -objs ' n]); 
+    status = system(['th '  sm_path  '/computeMasks.lua -res ' res_path ' -v 1 -img ' img_path  ' -objs ' n ' ' sm_path '/pretrained/sharpmask/']);
   elseif v == 2
-    status = system(['th '  sm_path  'computeMasks.lua -model ' sm_path '/pretrained/sharpmask/  -res ' res_path ' -v 2 -img ' img_path  ' -mask ' motion_path]); 
+    status = system(['th '  sm_path  '/computeMasks.lua -res ' res_path ' -v 2 -img ' img_path  ' -mask ' motion_path   ' ' sm_path '/pretrained/sharpmask/']);
   end
 else
-  fprintf('Image file does not exist..'); 
+  fprintf('Image file does not exist..');
 
 if status ~= 0
-  fprintf('Sharpmask did not succeed in creating masks'); 
+  fprintf('Sharpmask did not succeed in creating masks');
 end
 
 end
+
